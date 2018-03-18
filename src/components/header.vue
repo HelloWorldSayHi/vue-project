5f87c2403fcd5518b2199d7c19e1f52fcab6410c
HelloWorldSayHi <985429066@qq.com>
2018-03-18 21:16:22 +0800

    第一第一次

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ msg }}</p>
+</template>
+
+<script>
+export default {
+  name: 'v-header',
+  data() {
+    return {
+      msg: '这是头部！！'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
